439ae4fb4b6552ea6f73d2a06e5d9497f21cdc8e
Sergey <Shalfeyyy1@gmail.com>
2021-07-14 01:13:58 +0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -5,23 +5,21 @@ var path = require('path');
 var Sequelize = require('sequelize');
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || 'development';
-var config = require(__dirname + '/../config/config.js').config.db;
-var db = {};
+var config = require(__dirname + '/../config/config.js').config;
+var Chat = require('./chat.js');
+var Pair = require('./pair.js');
+var Reply = require('./reply.js');
+var Word = require('./word.js');
+var db = { Chat, Pair, Reply, Word };
 
-if (config.use_env_variable) {
-    var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-    var sequelize = new Sequelize(config.database, config.username, config.password, config);
+var sequelize = new Sequelize(config.dbUrl);
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
 }
-
-fs.readdirSync(__dirname)
-    .filter(function (file) {
-        return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
-    })
-    .forEach(function (file) {
-        var model = sequelize['import'](path.join(__dirname, file));
-        db[model.name] = model;
-    });
 
 Object.keys(db).forEach(function (modelName) {
     if (db[modelName].associate) {
@@ -30,6 +28,5 @@ Object.keys(db).forEach(function (modelName) {
 });
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
 
 module.exports = db;
